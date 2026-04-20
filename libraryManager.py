beda7a1824364381e2ffacd9bb5dfc9dada6aa2e
laypatel13 <laypatel1322008@gmail.com>
2026-04-20 22:13:15 +0530

    define Book class

--- libraryManager.py
+++ libraryManager.py
@@ -2,7 +2,18 @@ import json
 import os
 
 class Book:
-    pass
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        self.is_read = False
+
+    def display(self):
+        status = "Read" if self.is_read else "Unread"
+        print(f'Title: {self.title} | Author: {self.author} | Year: {self.year}')
+
+    def mark_as_read(self):
+        self.is_read = True
 
 class Library:
     pass
